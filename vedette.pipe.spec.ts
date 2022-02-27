2f46973edf18603c7af3bde243d24c6aede97599
Julien Hébert <73999978+JulienHebert855@users.noreply.github.com>
2022-02-26 23:23:35 -0500

    Delete vedette.pipe.spec.ts

--- vedette.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { VedettePipe } from './vedette.pipe';
-
-describe('VedettePipe', () => {
-  it('create an instance', () => {
-    const pipe = new VedettePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
