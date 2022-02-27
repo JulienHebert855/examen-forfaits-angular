997c76ccdb5a10482f37a194165dba0b2310b4c6
Julien Hébert <73999978+JulienHebert855@users.noreply.github.com>
2022-02-26 23:23:27 -0500

    Delete moncton.pipe.spec.ts

--- moncton.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MonctonPipe } from './moncton.pipe';
-
-describe('MonctonPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MonctonPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
