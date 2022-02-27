7135bf5667cf5c7530ea2d03f2efee27fe77ee2b
Julien Hébert <73999978+JulienHebert855@users.noreply.github.com>
2022-02-26 23:25:34 -0500

    Add files via upload

--- src/app/forfait.service.ts
+++ src/app/forfait.service.ts
@@ -1,25 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Forfait } from './forfait';
 import { Observable } from 'rxjs';
-
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForfaitService {
-  forfaitsUrl = 'http://localhost/projet-2-api/';
+  forfaitsurl = 'http://localhost/projet-2-api/';
 
   constructor(private http: HttpClient) { }
 
   getForfaits(): Observable<Forfait[]> {
-    return this.http.get<Forfait[]>(this.forfaitsUrl);
-  }
-
-  addForfait(forfait: Forfait): Observable<Forfait[]> {
-    return this.http.post<Forfait[]>(this.forfaitsUrl, forfait, httpOptions);
+    return this.http.get<Forfait[]>(this.forfaitsurl);
   }
 }
