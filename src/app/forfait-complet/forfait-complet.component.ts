eb6eb356c556b6ac5ddc8a68c06927c592a3ef55
Julien Hébert <73999978+JulienHebert855@users.noreply.github.com>
2022-02-26 23:15:53 -0500

    Add files via upload

---
+++ src/app/forfait-complet/forfait-complet.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Forfait } from '../forfait';
+
+@Component({
+  selector: 'app-forfait-complet',
+  templateUrl: './forfait-complet.component.html',
+  styleUrls: ['./forfait-complet.component.css']
+})
+export class ForfaitCompletComponent implements OnInit {
+  @Input() forfait: Forfait;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
